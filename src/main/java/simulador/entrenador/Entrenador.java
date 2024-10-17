684018702f38c3f0e82e1f2131f4487adf75b335
Neutron <neutronwar34@gmail.com>
2024-10-17 09:28:05 -0500

    Arreglo lista entrenador

--- src/main/java/simulador/entrenador/Entrenador.java
+++ src/main/java/simulador/entrenador/Entrenador.java
@@ -1,6 +1,8 @@
 package simulador.entrenador;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Entrenador {
     private String nombre;
-    List <pokemones>
+    List<String> pokemones = new ArrayList<>();
 }
