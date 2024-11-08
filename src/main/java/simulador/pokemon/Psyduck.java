ed0e73857e32907cbd0d2c18bf1a18340a5e317b
user <user@email.com>
2024-11-08 14:16:31 -0500

    Adicion psyduck y mejora clases + batalla
    Creación primera cllase de primer pokemon y mejora entrenador y pokemon, ademas de agregar batalla incompleta

---
+++ src/main/java/simulador/pokemon/Psyduck.java
@@ -0,0 +1,22 @@
+
+package simulador.pokemon;
+
+
+public class Psyduck extends Pokemon {
+    
+    public Psyduck (){
+        super("Psyduck", 55, 70, TipoPokemon.Agua);
+    }
+    
+    @Override
+    public void atacar(Pokemon oponente){
+         System.out.println(this.getNombre() + " ataca");
+        super.atacar(oponente);
+    }
+    
+    @Override
+    public void entrenar() {
+        System.out.println(this.getNombre() + " se ha entrenado con fuerza.");
+        super.entrenar();
+    }
+}
